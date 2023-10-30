c58b8bd794736b7ff414ff8ea49e1a5f661095a3
delfinajimenezherrera <djimenezherrera@udesa.edu.ar>
2023-10-30 10:03:19 -0300

    carpetas

---
+++ my-app/src/components/FormRegister.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react'
+import {auth} from '../../firebase/config';
+
+export default class FormRegister extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            name: '',
+            email:'',
+            password: ''
+        }
+    }
+
+    registrarUsuario(email, password){
+        auth.createUserWithEmailAndPassword(email, password)
+        .then(response => {
+            this.setState({registered: true});
+        })
+        .catch(error => {
+            this.setState({error: 'Fallo en el registro'})
+        })
+        }
+}
