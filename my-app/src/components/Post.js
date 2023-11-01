98f1d9894341d89addc6c50f62b2909ef7c42a2a
“guillescarcella” <scarcellag@udesa.edu.ar>
2023-11-01 08:50:35 -0300

    home

---
+++ my-app/src/components/Post.js
@@ -0,0 +1,16 @@
+import {Text, View, TouchableOpacity, StyleSheet} from 'react-native'
+import React, {Component} from 'react'
+
+export default class Post extends Component {
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <Text>
+                Post
+            </Text>
+        )
+    }
+}
